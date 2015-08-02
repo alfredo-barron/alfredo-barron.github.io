1784a3fdbe5cfd3b6bf47fed443b5a4e27d9050e
Alfredo Barrón Rodríguez <alfreedobarron@gmail.com>
2015-08-02 02:25:45 -0500

    profile

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1>Alfredo Barrón Rodríguez</h1>
-<p>Practicas y Tareas relizadas en la materia de Programación WEB</p>
--- public/js/js/game.js
+++
@@ -1,9 +0,0 @@
-var game = new Phaser.Game(w, h, Phaser.AUTO, 'div_game');
-
-
-game.state.add('Boot', Game.Boot);
-game.state.add('Load', Game.Load);
-game.state.add('Menu', Game.Menu);
-game.state.add('Play', Game.Play);
-
-game.state.start('Boot');
